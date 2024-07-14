5b59f071bf29bfbee613b819cd823cc05f268f26
Nicholas Thompson <NCGThompson@gmail.com>
2024-07-14 19:40:29 -0400

    Add iat method

--- src/fancy_map.py
+++ src/fancy_map.py
@@ -1,5 +1,6 @@
+import weakref
 from collections.abc import Hashable
-from typing import Iterator, Self
+from typing import Iterator, Self, SupportsIndex
 
 from simple_map import SimpleHashmap, _SimpleRecord
 
@@ -7,6 +8,7 @@ from simple_map import SimpleHashmap, _SimpleRecord
 class _FancyRecord[_KeyType: Hashable, _ValueType](_SimpleRecord[_KeyType, _ValueType]):
     next_ordered: Self | None
     prev_ordered: Self | None
+    l_index: int | None
 
     def __init__(self, key: _KeyType, value: _ValueType) -> None:
         super().__init__(key, value)
@@ -17,12 +19,14 @@ class _FancyRecord[_KeyType: Hashable, _ValueType](_SimpleRecord[_KeyType, _Valu
 class FancyHashmap[_KeyType: Hashable, _ValueType](SimpleHashmap[_KeyType, _ValueType]):
     _head: _FancyRecord[_KeyType, _ValueType] | None
     _tail: _FancyRecord[_KeyType, _ValueType] | None
+    _array: list[weakref.ref[_FancyRecord[_KeyType, _ValueType]]]
     # FIXME: Safely make _map covariant
 
     def __init__(self, size: int | None = None) -> None:
         super().__init__(size or 512)
         self._head = None
         self._tail = None
+        self._array = []
 
     def __setitem__(self, key: _KeyType, value: _ValueType) -> None:
         rec, p = self._find(key)
@@ -33,6 +37,7 @@ class FancyHashmap[_KeyType: Hashable, _ValueType](SimpleHashmap[_KeyType, _Valu
 
         assert isinstance(p, int)
 
+        # Add to hashmap
         node = _FancyRecord[_KeyType, _ValueType](key, value)
         m = self._map[p]
         if m is not None and not isinstance(m, _FancyRecord):
@@ -40,6 +45,7 @@ class FancyHashmap[_KeyType: Hashable, _ValueType](SimpleHashmap[_KeyType, _Valu
         node.next_collision = m
         self._map[p] = node
 
+        # Add to linked list
         if self._tail is None:
             assert self._head is None
             self._head = node
@@ -49,18 +55,25 @@ class FancyHashmap[_KeyType: Hashable, _ValueType](SimpleHashmap[_KeyType, _Valu
         node.prev_ordered = self._tail
         self._tail = node
 
+        # Add to array
+        self._array.append(weakref.ref(node))
+        node.l_index = self._count
+
         self._count += 1
 
     def __delitem__(self, key: _KeyType) -> None:
         rec, p = self._find(key)
         if rec is None:
-            # Should we error or return?
+            # FIXME: Should we error or return?
             return
+
+        # Remove from hashmap
         if isinstance(p, _SimpleRecord):
             p.next_collision = rec.next_collision
         else:
             self._map[p] = rec.next_collision
 
+        # Remove from linked list
         if rec.next_ordered is None:
             assert self._tail is rec
             self._tail = rec.prev_ordered
@@ -74,6 +87,13 @@ class FancyHashmap[_KeyType: Hashable, _ValueType](SimpleHashmap[_KeyType, _Valu
             assert rec.prev_ordered.next_ordered is rec
             rec.prev_ordered.next_ordered = rec.next_ordered
 
+        # Remove from array
+        a = self._array  # just to save letters
+        i = rec.l_index
+        assert isinstance(i, int)
+        a[-1], a[i] = a[i], a[-1]
+        a.pop()
+
         self._count -= 1
 
     def __iter__(self) -> Iterator[_KeyType]:
@@ -81,6 +101,18 @@ class FancyHashmap[_KeyType: Hashable, _ValueType](SimpleHashmap[_KeyType, _Valu
         while node is not None:
             yield node.key
             node = node.next_ordered
+
+    def iat(self, index: SupportsIndex) -> tuple[_KeyType, _ValueType]:
+        """Select a key value pair from a given integer.
+
+        For speed, this gets an entry from an internal list.
+        However, that list is not guaranteed to be in any particular
+        order, and can change arbitrily whenever an entry is added to
+        or removed from the mapping.
+
+        If the order of items matter, use the `iter` function instead."""
+        rec = self._by_index(index)
+        return (rec.key, rec.value)
 
     def _find(
         self, key: _KeyType
@@ -94,3 +126,9 @@ class FancyHashmap[_KeyType: Hashable, _ValueType](SimpleHashmap[_KeyType, _Valu
         if not isinstance(b, (_FancyRecord, int)):
             raise TypeError
         return (a, b)
+
+    def _by_index(self, index: SupportsIndex) -> _FancyRecord[_KeyType, _ValueType]:
+        rec = self._array[index]()
+        if rec is None:
+            raise ReferenceError
+        return rec
